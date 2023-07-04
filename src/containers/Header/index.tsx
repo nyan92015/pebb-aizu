ac6e5033be3fc9934cace0d180405e31e6bb49cb
nyan92015 <itoyan15@icloud.com>
2023-07-05 00:53:07 +0900

    scssファイルを作ったよ

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -2,12 +2,12 @@ import Logo from "../../components/Logo";
 import SignOut from "../SignOut";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-
+import "./Header.scss";
 const Header = () => {
   const user = useSelector((state: RootState) => state.user.userData);
 
   return (
-    <div>
+    <div className="header">
       <Logo />
       <p>{user?.displayName}</p>
       {user?.emailVerified && <SignOut />}
